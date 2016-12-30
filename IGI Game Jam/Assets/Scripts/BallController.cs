cbca7b49e152ceb68e8dbceb5fea73ddd32f3daa
hoddi84 <hordurh15@ru.is>
2016-12-30 21:58:09 +0000

    Initial commit, with 1 test scene

---
+++ IGI Game Jam/Assets/Scripts/BallController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    private Vector3 down;
+
+    private bool canJump;
+    public float jumpPower;
+
+	void Start () {
+
+        rb = GetComponent<Rigidbody>();
+        down = new Vector3(0, -1);
+	}
+	
+	void Update () {
+		
+	}
+
+    void FixedUpdate ()
+    {
+        /* 
+         * Using raycast to detect if we are touching the ground
+         * to limit our jumping.  
+         */
+        if (Physics.Raycast(transform.position, down, 0.151f))
+        {
+            canJump = true;
+        }
+        else
+        {
+            canJump = false;
+        }
+
+        /* 
+         * Simple controls for movement using velocity change. 
+         */
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector3(-1, rb.velocity.y);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector3(1, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector3(0, rb.velocity.y);
+        }
+
+        if (Input.GetKey(KeyCode.Space) && canJump)
+        {
+            rb.AddForce(0, jumpPower, 0, ForceMode.Impulse);
+        }
+    }
+}
