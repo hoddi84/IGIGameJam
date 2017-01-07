d1f0578436632c28481a3f30af7b13213700a7cc
Brynjar Eddi Rafnarsson <brynjarr@vodafone.is>
2017-01-07 12:51:32 +0000

    Added start screen

---
+++ IGI Game Jam/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public bool isStart;
+    public bool isQuit;
+
+    void OnMouseUp()
+    {
+        if (isStart)
+        {
+            //Application.LoadLevel("jinjang");
+            SceneManager.LoadScene("jinjang");
+        }
+        if (isQuit)
+        {
+            Application.Quit();
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ IGI Game Jam/Assets/Scripts/MouseHover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHover : MonoBehaviour {
+
+    void Start()
+    {
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+
+    void OnMouseEnter()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        
+    }
+}
