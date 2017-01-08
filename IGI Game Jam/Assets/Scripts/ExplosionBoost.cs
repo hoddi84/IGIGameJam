a9ce534c6f2eca71ed8c097b1150afe23f9fd21c
hoddi84 <hordurh15@ru.is>
2017-01-08 10:59:27 +0000

    working explosion pickable

---
+++ IGI Game Jam/Assets/Scripts/ExplosionBoost.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionBoost : MonoBehaviour
+{
+
+    public float speed;
+    private Controller controller;
+
+    private ExploderController whiteExploController;
+    private ExploderController blackExploController;
+
+    // Use this for initialization
+    void Start()
+    {
+        controller = GameObject.Find("GameController").GetComponent<Controller>();
+
+        whiteExploController = GameObject.Find("WhitePlayer").GetComponent<ExploderController>();
+        blackExploController = GameObject.Find("BlackPlayer").GetComponent<ExploderController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Rotate(0, 1 * speed, 0, Space.Self);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "WhitePlayer" && controller.ControlingWhite())
+        {
+            whiteExploController.PickUpExplosion();
+            Destroy(gameObject);
+        }
+        else if (other.gameObject.tag == "WhitePlayer" && !controller.ControlingWhite())
+        {
+            // something bad.
+            Destroy(gameObject);
+        }
+        if (other.gameObject.tag == "BlackPlayer" && !controller.ControlingWhite())
+        {
+            blackExploController.PickUpExplosion();
+            Destroy(gameObject);
+        }
+        else if (other.gameObject.tag == "BlackPlayer" && controller.ControlingWhite())
+        {
+            // something bad.
+            Destroy(gameObject);
+        }
+    }
+}
